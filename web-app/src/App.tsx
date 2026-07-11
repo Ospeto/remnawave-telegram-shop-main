aa28d6f06ae8fe41b7609d0293d04931f45619f5
Ospeto <whisper.of.a.rose2012@gmail.com>
2026-07-12 05:38:23 +0630

    feat(web): register /admin/finance and Home navigation card

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -5,6 +5,7 @@ import { Checkout } from './pages/Checkout';
 import { Wallet } from './pages/Wallet';
 import { AdminPromos } from './pages/AdminPromos';
 import { AdminPlans } from './pages/AdminPlans';
+import { AdminFinance } from './pages/AdminFinance';
 
 import { ThemeProvider } from './lib/ThemeProvider';
 
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/admin/promos" element={<AdminPromos />} />
                     <Route path="/admin/plans" element={<AdminPlans />} />
+                    <Route path="/admin/finance" element={<AdminFinance />} />
                     <Route path="/plans" element={<Plans />} />
                     <Route path="/wallet" element={<Wallet />} />
                     <Route path="/checkout" element={<Checkout />} />
